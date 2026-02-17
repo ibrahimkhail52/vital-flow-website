9e641cfdfbc817693dbbb6acbf848d28a98a4cde
Basir-H <basirhabibi705@gmail.com>
2026-02-17 16:18:36 +0430

    Features Section completed

---
+++ src/assets/features img/index.js
@@ -0,0 +1,18 @@
+import careNetwork from './careNetwork.svg';
+import consultationsy from './consultationsy.svg';
+import easyAppoinment from './easyAppoinment.svg'
+import medical from './medical.svg';
+import patient from './patient.svg';
+import personalizedPlans from './personalizedPlans.svg';
+
+
+
+export{
+    careNetwork,
+    consultationsy,
+    easyAppoinment,
+    medical,
+    patient,
+    personalizedPlans
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,11 @@ export default {
 
     screens: {
       vs:'375px', 
+      sm: '640px',
       md:'768px',
       mmd:'820px',
+      lg: '1024px',
+      xl:'1280px ',
     },
   },
    
