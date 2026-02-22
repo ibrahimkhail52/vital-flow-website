6770a932f9962710a7cb5366e462dd286b248d23
irfanKhan2025721 <irfanahmadzai1900@gmail.com>
2026-02-22 11:21:09 +0430

    Refactor buttons to use AnimatedButton component for consistency across About, WhyUs, and PartnerInHealth pages; remove unused navigate hooks and update index.js with new component exports.

--- src/Components/About Page/AboutUs/About.jsx
+++ src/Components/About Page/AboutUs/About.jsx
@@ -1,10 +1,12 @@
 import { aboutHeroData } from "../AboutUs/aboutUsData";
 import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom";
+
+
+import AnimatedButton from "../../AnimatedButton/AnimatedButton";
 
 const About = () => {
   const { badge, title, buttonText } = aboutHeroData;
-const navigate = useNavigate()
+
   return (
     <section className="max-w-4xl mx-auto px-6 pt-20 text-center">
       {/* Badge */}
@@ -29,19 +31,12 @@ const navigate = useNavigate()
       </motion.h1>
 
       {/* Button */}
-      <button
-        onClick={() => navigate("/Book-call")}
-        className="group relative overflow-hidden rounded-full bg-[#4686FF] px-6 py-3 text-white font-semibold transition-transform duration-300"
-      >
-        <span className="relative block overflow-hidden">
-          <span className="block transition-transform duration-500 ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:-translate-y-full">
-            Get Started
-          </span>
-          <span className="absolute inset-0 flex items-center justify-center translate-y-full transition-transform duration-700 ease-[cubic-bezier(0.22,1,0.36,1)] group-hover:translate-y-0">
-            Get Started 
-          </span>
-        </span>
-      </button>
+      <AnimatedButton
+        text={buttonText}
+        to="/book-call"
+        bgColor="#4686FF"
+        className="inline-block"
+      />
     </section>
   );
 };
